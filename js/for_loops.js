e89adbd0b588f47d623b741b67d8a5b54eb941f0
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-09-26 13:15:37 -0500

    added new while loop
    ignored, and an empty message aborts the commit. # # On branch main # Your branch is
    ahead of 'origin/main' by 1 commit. # (use "git push" to publish your local commits) # #
    Changes to be committed: # modified:  js/for_loops.js #

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,14 +8,30 @@ function showMultiplicationTable(number) {
 console.log(showMultiplicationTable(7));
 
 
-var randomNum = Math.floor(Math.random() * 181) + 20;
+// var randomNum = Math.floor(Math.random() * 181) + 20;
+//
+// for (var i = 0; i < 500; i++) {
+//     var randomNum = Math.floor(Math.random() * 180) + 20;
+//     if (randomNum < 20 || randomNum > 200) {
+//         console.log(("What's up"));
+//     } else if (randomNum === 20 || randomNum === 200) {
+//         console.log("sooooo");
+//     }
+//     console.log(randomNum);
+// }
+//
+//
+// for (var i = 100; i >= 5; i -= 5){
+//     console.log(i);
+// }
 
-for (var i = 0; i < 500; i++) {
-    var randomNum = Math.floor(Math.random() * 180) + 20;
-    if (randomNum < 20 || randomNum > 200) {
-        console.log(("What's up"));
-    } else if (randomNum === 20 || randomNum === 200) {
-        console.log("sooooo");
+
+for (var number = 1; number < 10; number++){
+    var count = 0;
+    var outputString = "";
+    while (count < number) {
+        outputString = outputString + number.toString();
+        count = count + 1;
     }
-    console.log(randomNum);
+    console.log(outputString);
 }
